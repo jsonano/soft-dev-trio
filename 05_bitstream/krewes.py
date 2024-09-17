2fea21c253b6034a825d34883fca159b8ffa4007
Jason Chao <jchao50@stuy.edu>
2024-09-17 13:47:39 -0400

    05_bitstream assignment

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -4,14 +4,19 @@
 # K05 - Processing Bitstreams
 # 2024 - 9 - 17
 # Time spent:
+import random as r
 
 dict = {}
 
 file = open("krewes.txt", "r")
-lst = file.read().split('@@@')
-lst = lst[0:len(lst) - 1]
+lst = file.read().split('@@@') # separating each devo using separator '@@@'
+lst = lst[0:len(lst) - 1] # removing "\n" from the end
 
 for profile in lst:
-    profileList = profile.split('$$$')
-    dict[profileList[1]] = profileList
+    profile_list = profile.split('$$$')  # separating devo info into a list | [pd, devo, ducky]
+    dict[profile_list[1]] = profile_list # assigning devo name to their info in dict | devo : [pd, devo, ducky]
 
+devo = r.choice(list(dict.keys()))
+devo_info = dict[devo]
+print(devo_info)
+print(f'Period: {devo_info[0]} \nName: {devo_info[1]} \nDucky: {devo_info[2]}\n')
