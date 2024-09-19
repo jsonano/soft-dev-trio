b8659b5a657d66e79418a57a050d8bf4a76c1b98
Jason Chao <jchao50@stuy.edu>
2024-09-19 13:48:07 -0400

    numbercruncher complete

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,4 +1,3 @@
-import random
 from random import choices
 
 dict = {}
@@ -8,11 +7,11 @@ with open('occupations.csv', 'r') as file:
         lst = line.strip().replace('"', '').rsplit(',', 1) # r.split(',', 1) only splits off the FIRST comma from the right
         dict[lst[0]] = lst[1] # occupation : percentage
 
+del dict['Job Class'] # removing "Job Class" labels
 del dict['Total'] # removing "Total" key-value pair
 
 all_occupations = list(dict.keys())
-occupation_weights = [x / 100 for x in dict.values()]
-for weight in dict.values():
-    occupation_weights.append(float(weight) / 100)
-print(choices(population = all_occupations, weights = occupation_weights))
-
+occupation_weights = [float(x) / 100 for x in dict.values()] # converting percentages into decimals | 5.5 --> 0.055
+# print(all_occupations)
+# print(occupation_weights)
+print(choices(all_occupations, weights = occupation_weights)) # random.choices(list, weighted_percentages)
