0a35cce5e5479ae8381c414798f1fb808fe37132
Jason Chao <jchao50@stuy.edu>
2024-10-11 10:46:32 -0400

    Added cookies

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,8 +1,10 @@
 # heading
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 logins = {"victor" : "casado",
           "jason" : "chao",
@@ -10,22 +12,30 @@ logins = {"victor" : "casado",
 
 @app.route('/')
 def login():
-    return render_template("login.html")
+    if 'username' in session:
+        return redirect('/auth')
+    else:
+        return render_template("login.html")
 
 @app.route('/auth')
 def auth_login():
+    if 'username' in session:
+        return render_template("home.html", user = session['username'])
     login = request.args
-    print(login['username'])
-    print(login['password'])
+    # print(login['username'])
+    # print(login['password'])
     username = login['username']
     password = login['password']
     if username in logins:
         if logins[username] == password:
+            session['username'] = username
             return render_template("home.html", user = username)
         else:
-            return render_template("login.html")
+            return redirect('/')
     else:
-        return render_template("login.html") # try to redirect back to route
+        return redirect('/') # redirect back to / route
+
+
 
 if __name__ == "__main__": #false if this file imported as module
     app.debug = True
