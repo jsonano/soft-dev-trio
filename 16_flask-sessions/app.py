bfd87ac53d643f700f5ef45b6dc17ab76da8faf5
jsonano <jchao5768@gmail.com>
2024-10-14 19:25:34 -0400

    Added styling to HTML files

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,10 +1,15 @@
-# heading
+# Victor Casado, Evan Chan, Jason Chao
+# The Flying Mice
+# 16 - Flask Sessions w/ Cookies
+# SoftDev
+# 2024-10-11
+# Time spent: 2 hours
 
 from flask import Flask, render_template, request, session, redirect
 import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
+app.secret_key = os.urandom(32) # generates a random secret key for session
 
 logins = {"victor" : "casado",
           "jason" : "chao",
@@ -13,7 +18,7 @@ logins = {"victor" : "casado",
 @app.route('/')
 def login():
     if 'username' in session:
-        return redirect('/auth')
+        return redirect('/auth') # automatically redirect to /auth route if user is already logged in
     else:
         return render_template("login.html")
 
@@ -22,10 +27,10 @@ def auth_login():
     if 'username' in session:
         return render_template("home.html", user = session['username'])
     login = request.args
-    # print(login['username'])
-    # print(login['password'])
     username = login['username']
     password = login['password']
+    # print(username)
+    # print(password)
     if username in logins:
         if logins[username] == password:
             session['username'] = username
@@ -33,7 +38,7 @@ def auth_login():
         else:
             return redirect('/')
     else:
-        return redirect('/') # redirect back to / route
+        return redirect('/') # redirect back to / route if username and/or password is incorrect
 
 
 
